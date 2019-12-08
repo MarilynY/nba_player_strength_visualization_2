15f58761000a61da32004ff868ae52b8db3fa84d
Menglin Yu <myu311@gatech.edu>
2019-12-08 00:05:31 -0500

    2nd commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import Profile from './Profile';
 import ShotChart from "./ShotChart";
+import nba from 'nba';
 
+console.log(nba);
 class Main extends Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {}
+    }
+
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
+            // console.log(info);
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            //console.log('playerInfo', playerInfo);
+            this.setState({ playerInfo });
+        })
+    }
+
     render() {
+        console.log(this.state.playerId);
         return (
-            <div>
-                <Profile />
+            <div className="main">
+                <Profile playerInfo={this.state.playerInfo}/>
                 <ShotChart />
             </div>
         );
